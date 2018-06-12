a884208fe811eff49385fdecf29c351426c6d79f
mnap <learn@10g.pl>
2018-06-12 23:05:29 +0200

    Add Lane's routes; adjust server script

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,5 +3,7 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
+// Add a new Lane
+router.route('/lanes').post(LaneController.addLane);
 
 export default router;
