fa3d4bb823898e1de9b3049e18b4f3f8cd558430
mnap <learn@10g.pl>
2018-06-13 21:16:02 +0200

    Add getLanes controller and route

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -2,6 +2,15 @@ import uuid from 'uuid';
 
 import Lane from '../models/lane';
 
+export function getLanes(req, res) {
+  Lane.find().exec((err, lanes) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ lanes });
+  });
+}
+
 export function addLane(req, res) {
   if (!req.body.name) {
     res.status(403).end();
