83cb987335f0649139da98e666cdeb85ad95dcc6
mnap <learn@10g.pl>
2018-06-13 21:53:55 +0200

    Add deleteLane controller and route

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -27,3 +27,15 @@ export function addLane(req, res) {
     res.json(saved);
   });
 }
+
+export function deleteLane(req, res) {
+  Lane.findOne({ id: req.params.laneId }).exec((err, lane) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    lane.remove(() => {
+      res.status(200).end();
+    });
+  });
+}
