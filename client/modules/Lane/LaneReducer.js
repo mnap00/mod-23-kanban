b569f4c9f383fa6dbd5ab8f9d8a4016f00fac263
mnap <learn@10g.pl>
2018-06-14 23:49:13 +0200

    Add client Lane reducers

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,11 +1,23 @@
 // Import Actions
-import {  } from './LaneActions';
+import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
 
 // Initial State
-const initialState = {};
+const initialState = [];
 
 const LaneReducer = (state = initialState, action) => {
   switch (action.type) {
+
+    case CREATE_LANE:
+      return [...state, action.lane];
+
+    case UPDATE_LANE:
+      return state.map(lane => {
+        return lane.id === action.id ? { ...lane, ...action.lane } : lane;
+      });
+
+    case DELETE_LANE:
+      return state.filter(lane => lane.id !== action.laneId);
+
     default:
       return state;
   }
