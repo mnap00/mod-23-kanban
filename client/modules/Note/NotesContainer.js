4bc984fa599e649bdb3d2bb243dc5ed2c7d6a48e
mnap <learn@10g.pl>
2018-06-15 21:01:54 +0200

    Add NotesContainer container

---
+++ client/modules/Note/NotesContainer.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+
+import * as noteActions from './NoteActions';
+import Notes from './Notes';
+
+const mapDispatchToProps = {
+  ...noteActions,
+};
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(Notes);
