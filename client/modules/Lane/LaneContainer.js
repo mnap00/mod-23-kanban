430d3d95508ef2ebfada7beb8e3ad75658e99b93
mnap <learn@10g.pl>
2018-06-15 20:52:23 +0200

    add LaneContainer container; update Lanes

---
+++ client/modules/Lane/LaneContainer.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+
+import * as laneActions from './LaneActions';
+import { createNote } from '../Note/NoteActions';
+import Lane from './Lane';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    laneNotes: ownProps.lane.notes.map(noteId =>
+      state.notes.find(note => note.id === noteId)),
+  };
+};
+
+const mapDispatchToProps = {
+  ...laneActions,
+  addNote: createNote,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Lane);
