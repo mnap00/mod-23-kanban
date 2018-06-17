0e5d461fe86859003ee846d7b1d135e09a31b0ab
mnap <learn@10g.pl>
2018-06-17 20:53:05 +0200

    Add drag source functionality to Note component

--- client/modules/Note/Note.js
+++ client/modules/Note/Note.js
@@ -1,13 +1,48 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { DragSource } from 'react-dnd';
+
+import ItemTypes from '../Kanban/itemTypes';
 
 // Import Style
 import styles from './Note.css';
 
-const Note = (props) =>
-  <li className={styles.Note}>{props.children}</li>;
+class Note extends Component {
+  render() {
+    const { connectDragSource, isDragging, editing, children } = this.props;
+    // no dragging when editing component
+    const dragSource = editing ? a => a : connectDragSource;
+
+    return dragSource(
+      <li
+        className={styles.Note}
+        style={{ opacity: isDragging ? 0 : 1 }}
+      >
+        {children}
+      </li>
+    );
+  }
+}
 
 Note.propTypes = {
+  connectDragSource: PropTypes.func,
+  isDragging: PropTypes.bool,
+  editing: PropTypes.bool,
   children: PropTypes.any,
 };
 
-export default Note;
+const noteSource = {
+  beginDrag(props) {
+    return {
+      id: props.id,
+      laneId: props.laneId,
+    };
+  },
+  isDragging(props, monitor) {
+    return props.id === monitor.getItem().id;
+  },
+};
+
+export default DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging(),
+}))(Note);
